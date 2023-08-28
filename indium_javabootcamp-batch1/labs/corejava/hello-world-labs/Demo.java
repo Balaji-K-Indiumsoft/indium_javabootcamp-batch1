98a4f917908be15012cfaa8511c42a9f2699c5bd
Balaji-K-Indiumsoft <balaji.K@indiumsoft.com>
2023-08-28 15:56:30 +0530

    upated code

--- indium_javabootcamp-batch1/labs/corejava/hello-world-labs/Demo.java
+++ indium_javabootcamp-batch1/labs/corejava/hello-world-labs/Demo.java
@@ -1,8 +1,14 @@
 public class Demo {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-		System.out.println("Hello User");
+        System.out.println("Hello User");
+        try {
+            // sleep for 5 mins
+            Thread.sleep(1000 * 60 * 5);
+        } catch (InterruptedException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
-	}
+    }
 }
