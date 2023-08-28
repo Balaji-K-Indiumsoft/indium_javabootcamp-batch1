19ca75332f59afb2936597c6b6f5f832f062d771
Balaji-K-Indiumsoft <balaji.K@indiumsoft.com>
2023-08-28 17:01:40 +0530

    vs commit

---
+++ indium_javabootcamp-batch1/labs/corejava/First-javaProgram/firstproject/src/main/java/com/example/java/Main.java
@@ -0,0 +1,11 @@
+package com.example.java;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int i;
+        for(i=0;i<=5;i++){
+            System.out.println(i);
+        }
+    }
+}
