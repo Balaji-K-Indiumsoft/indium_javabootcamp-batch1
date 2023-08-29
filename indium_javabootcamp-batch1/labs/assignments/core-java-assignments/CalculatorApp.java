95164960acd0636e5743a0a226ec216ee873a59a
Balaji-K-Indiumsoft <balaji.K@indiumsoft.com>
2023-08-29 12:30:05 +0530

    Calculator assignment commit

---
+++ indium_javabootcamp-batch1/labs/assignments/core-java-assignments/CalculatorApp.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class CalculatorApp {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to the Calculator App!");
+        System.out.println("-------------------------------");
+
+        char operator;
+        double number1, number2, result;
+
+        System.out.print("Enter first number: ");
+        number1 = input.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /): ");
+        operator = input.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        number2 = input.nextDouble();
+
+        switch (operator) {
+            case '+':
+                result = number1 + number2;
+                System.out.println("Result: " + number1 + " + " + number2 + " = " + result);
+                break;
+            case '-':
+                result = number1 - number2;
+                System.out.println("Result: " + number1 + " - " + number2 + " = " + result);
+                break;
+            case '*':
+                result = number1 * number2;
+                System.out.println("Result: " + number1 + " * " + number2 + " = " + result);
+                break;
+            case '/':
+                if (number2 != 0) {
+                    result = number1 / number2;
+                    System.out.println("Result: " + number1 + " / " + number2 + " = " + result);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator.");
+                break;
+        }
+
+        System.out.println("-------------------------------");
+        System.out.println("Thank you for using the Calculator App!");
+        input.close();
+    }
+}
